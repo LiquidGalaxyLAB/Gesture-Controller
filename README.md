a7e7fe2b32482af2d81711a5e707771c1339722e
brunofaefaion <bruno.fae.faion@gmail.com>
2020-06-04 17:15:52 -0300

    Socketio bases implemented

--- README.md
+++ README.md
@@ -3,17 +3,17 @@
 - Android Studio
 - Flutter
 
-###### Test:
+##### Test:
 1. Install the pub packages `flutter packages get`
 2. Inside gcmobile foder run `flutter run`
 
 
 
 ### GC Server
-###### Requirements:
+##### Requirements:
 - NodeJs
 - npm
 
-###### Test:
+##### Test:
 1. Get all npm packages with `npm install`
 2. Inside gcserver folder run server `node index.js`
--- gcserver/index.js
+++ gcserver/index.js
@@ -1,9 +1,17 @@
 const app = require('express')()
+const http = require('http').createServer(app)
+const socketio = require('socket.io')(http)
 
 const port = 8080
 
 app.get('/', (req,res)=>{
   res.send('Root route.')
+})
+
+socketio.on("connection", (userSocket) => {
+  userSocket.on("send_message", (data) => {
+    userSocket.broadcast.emit("receive_message",data)
+  })
 })
 
 app.listen(port,() => console.log(`GC Server listening on port ${port}`))
