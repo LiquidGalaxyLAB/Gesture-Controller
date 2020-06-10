ea13c18aa149f94a4192fa68f2a022618767cce4
brunofaefaion <bruno.fae.faion@gmail.com>
2020-06-10 18:18:46 -0300

    Remove headers params

--- gcserver/index.js
+++ gcserver/index.js
@@ -5,10 +5,10 @@ const socketio = require('socket.io')(http)
 const port = 8080
 
 app.get('/', (req,res)=>{
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  // res.header("Access-Control-Allow-Headers", "Content-Type");
+  // res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   res.send('Root route.')
 })
 
