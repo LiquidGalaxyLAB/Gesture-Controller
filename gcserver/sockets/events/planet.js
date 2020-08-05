2ad04fd9ba55e2430ee3ab0ddfa4ec30e15320c9
brunofaefaion <bruno.fae.faion@gmail.com>
2020-08-05 01:21:46 -0300

    Change server planet command

--- gcserver/sockets/events/planet.js
+++ gcserver/sockets/events/planet.js
@@ -1,10 +1,11 @@
 const exec = require('child_process').exec;
 const {query} = require(`../../utils/commands`);
 
-function planet(data){
+function planet(data, laststate){
   console.log('change planet ->', data)
   operation = 'planet'
-  exec(`${query(operation, data.direction)}`)
+  exec(`${query(operation, data.direction, laststate)}`)
+  laststate=''
 }
 
 module.exports = {
