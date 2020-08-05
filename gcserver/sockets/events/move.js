09f1bbc6cf294990916741c64d27ba42cc5cccf2
brunofaefaion <bruno.fae.faion@gmail.com>
2020-08-05 01:17:45 -0300

    Some code clear up

--- gcserver/sockets/events/move.js
+++ gcserver/sockets/events/move.js
@@ -2,12 +2,11 @@ const exec = require('child_process').exec;
 const {command} = require(`../../utils/commands`);
 
 function move(data, lastState){
-  console.log(lastState)
   console.log('move ->', data)
   key = `move_${data.direction}`
   if(lastState.state!=key)
     exec(`${command(key,lastState.state)}`)
-  lastState.state=`move_${data.direction}`
+  lastState.state=key
 }
 
 module.exports = {
