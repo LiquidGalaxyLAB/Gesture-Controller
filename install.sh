9d83895658864f0e821d6f089781c56c71e58262
brunofaefaion <bruno.fae.faion@gmail.com>
2020-08-06 01:10:45 -0300

    Added iptables on installer

--- install.sh
+++ install.sh
@@ -26,6 +26,9 @@ echo -n "" > .env
 echo SERVER_PORT=$SERVER_PORT >> .env
 echo MASTER_IP=$MASTER_IP >> .env
 
+iptables -I INPUT 1 -i eth0 -p tcp --dport $SERVER_PORT -j ACCEPT
+iptables-save
+
 # ssh-keygen -f /tmp/gcserver -t rsa -b 4096 -q -N "" -C "$SUDO_USER@$HOSTNAME"
 # ssh-keygen -t rsa -f gcserver
 
