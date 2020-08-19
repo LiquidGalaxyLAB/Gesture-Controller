d5361b42de19c0c953378deb6777e9fffa54a77b
brunofaefaion <bruno.fae.faion@gmail.com>
2020-08-18 23:28:23 -0300

    Logos

--- gcserver/index.js
+++ gcserver/index.js
@@ -3,6 +3,7 @@ const http = require('http').createServer(app)
 const socketio = require('socket.io')(http)
 const {connection} = require('./sockets/index.js')
 const dotenv = require('dotenv');
+const exec = require('child_process').exec;
 
 dotenv.config();
 const port = process.env.SERVER_PORT;
@@ -20,5 +21,18 @@ app.get('/', (req,res)=>{
 
 socketio.on("connection", connection)
 
+// function openLogos(screen){
+//   if(MASTER_IP != 'localhost'){
+//     exec(`ssh lg@${MASTER_IP} 'export DISPLAY=:0;mkdir gc-assets;ssh lg@${screen} "export DISPLAY=:0;mkdir gc-assets"'`)
+//     exec(`scp ./utils/logos.png lg@${MASTER_IP}:~/gc-assets/`)
+//     exec(`ssh lg@${MASTER_IP} 'export DISPLAY=:0;scp ~/gc-assetsr/logos.png lg@lg${screen}:~/gc-assets/'`)
+//     exec(`ssh lg@${MASTER_IP} 'export DISPLAY=:0;ssh lg@lg${screen} "export DISPLAY=:0; feh -x -g 400x400 ~/gc-assets/logos.png"'`)
+//   } else{
+//     exec(`scp utils/logos.png lg@lg${screen}:~/GestureController/`)
+//     exec(`ssh lg${screen} "export DISPLAY=:0;feh -x -g 400x400 ~GestureController/logos.png"`)
+//   }
+// }
 
-http.listen(port,() => console.log(`GC Server listening on port ${port}`))
+http.listen(port,() => {
+  console.log(`GC Server listening on port ${port}`)
+})
